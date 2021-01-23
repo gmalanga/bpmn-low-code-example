72824d71be4a8ab4c77e369af31ca9bfa2457b2a
Giuseppe Malanga <gmalanga@zaizi.com>
2021-01-23 18:34:34 +0100

    Reorganised the project structure.

---
+++ generate-boarding-pass/src/main/java/com/zaizi/Application.java
@@ -0,0 +1,17 @@
+package com.zaizi;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@EnableProcessApplication
+public class Application {
+
+	public static void main(String... args) {
+	    SpringApplication.run(Application.class, args);
+	  }
+
+}
